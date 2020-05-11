c5585f02ab061ddb3b27f8105749fb8047142f60
Kevin.Tung <chen0625tung@gmail.com>
2020-05-11 17:54:01 +0800

    initial commit

---
+++ helpers.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+const helpers = {
+    getDateStrArray: timeStr => {
+        const year = timeStr.substring(0, 4);
+        const month = timeStr.substring(4, 6);
+        const day = timeStr.substring(6);
+        return [year, month, day];
+    },
+    getNextDateOfMonth: timeStr => {
+        const year = timeStr.substring(0, 4);
+        const month = timeStr.substring(4, 6);
+
+        let nextMonth = parseInt(month) + 1;
+        if(nextMonth > 12) {
+            nextMonth = '01';
+            const nextYear = parseInt(year) + 1;
+            return [nextYear.toString(), nextMonth, '10'];
+        }
+        return [year, nextMonth.toString(), '10'];
+    },
+    getAllExpense: () => {
+        const fileContents = fs.readFileSync('expenses.txt', 'utf8')
+        let total = 0;
+        const fileArr = fileContents.split('\r\n').filter(line => !line.startsWith('//'));
+        fileArr.forEach(line => {
+            const expDetl = line.split('=');
+            total += parseInt(expDetl[1]);
+        })
+        return [total, fileArr];
+    }
+}
+
+module.exports = helpers;
---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const exphbs = require('express-handlebars');
+const helpers = require('./helpers');
+
+const app = express();
+
+// set middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// set view engine
+app.engine('handlebars', exphbs({
+    extname: 'handlebars',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/views/layouts/',
+    partialsDir: __dirname + '/views/partials/'
+}));
+app.set('view engine', 'handlebars');
+
+// routes
+app.get('/', (req, res) => {
+    const [total, allExp] = helpers.getAllExpense();
+    const tableContent = allExp.map(e => {
+        contentDetl = e.split('=');
+        return { name: contentDetl[0], cost: contentDetl[1] }
+    });
+    res.render('home', { total, allExp: tableContent })
+});
+
+app.post('/calculate', (req, res) => {
+    let jsonMsg = {};
+    const income = req.body.income;
+    const currentDate = req.body.currentDate;
+    if(income == null || income == undefined || income.length === 0) {
+        jsonMsg.msg = 'income required!';
+    }
+
+    const [nextYear, nextMonth, nextDay] = helpers.getNextDateOfMonth(currentDate);
+    const [year, month, day] = helpers.getDateStrArray(currentDate);
+    const date = new Date(year, parseInt(month) - 1, day);
+    const nextDate = new Date(nextYear, parseInt(nextMonth) - 1, nextDay);
+    const diff = nextDate.getTime() - date.getTime();
+    const diffInDays = diff / 1000 / 60 / 60 / 24;
+    const [total, allExp] = helpers.getAllExpense();
+    const dailyAverage = (parseInt(income) - total) / diffInDays;
+    jsonMsg.daily = dailyAverage;
+    jsonMsg.balance = parseInt(income) - total;
+
+    res.json(jsonMsg);
+});
+
+// listen to port
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`server listening to ${port}...`))

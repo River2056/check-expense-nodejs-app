963758eda4ff06b45959928bdbd43452c8d7f6f1
Kevin.Tung <chen0625tung@gmail.com>
2020-05-13 11:00:17 +0800

    change helpers.js line split pattern for play.js to use

--- helpers.js
+++ helpers.js
@@ -22,7 +22,7 @@ const helpers = {
     getAllExpense: () => {
         const fileContents = fs.readFileSync('expenses.txt', 'utf8')
         let total = 0;
-        const fileArr = fileContents.split('\r\n').filter(line => !line.startsWith('//'));
+        const fileArr = fileContents.split('\n').filter(line => !line.startsWith('//'));
         fileArr.forEach(line => {
             const expDetl = line.split('=');
             total += parseInt(expDetl[1]);
